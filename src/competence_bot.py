b2b52ff263f4a8de1ca17b8f0e5e3b379e0917e6
i.meleshenkov <i.meleshenkov@infinnity.ru>
2023-05-26 10:37:10 +0500

    [update]: change start bot and change 1c report view

--- src/competence_bot.py
+++ src/competence_bot.py
@@ -4,4 +4,12 @@ from os import environ
 BOT_TOKEN = environ["BOT_TOKEN"]
 
 competence_bot = bot.Competence_bot(bot_token=BOT_TOKEN)
-competence_bot.run()
+def main():
+    while True:
+        try:
+            competence_bot.run()
+        except Exception as e:
+            continue
+
+if __name__=='__main__':
+    main()
